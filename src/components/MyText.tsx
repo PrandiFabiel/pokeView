51befef9357f74d13b105594d33ab976752a372b
PrandiFabiel <fabiel098@gmail.com>
2022-12-13 08:33:55 -0400

    Eliminando fuentes que no se utilizaban.

--- src/components/MyText.tsx
+++ src/components/MyText.tsx
@@ -1,24 +1,12 @@
 import { StyleSheet, Text } from "react-native";
-import {
-  useFonts,
-  Roboto_300Light,
-  Roboto_400Regular,
-} from "@expo-google-fonts/roboto";
-import { Oswald_300Light } from "@expo-google-fonts/oswald";
-import {
-  OpenSans_400Regular,
-  OpenSans_300Light,
-} from "@expo-google-fonts/open-sans";
+import { useFonts } from "@expo-google-fonts/roboto";
+import { OpenSans_400Regular } from "@expo-google-fonts/open-sans";
 import React from "react";
 import { IMyText } from "../interfaces/IMyText";
 
-const CustomText = ({numberOfLines, text, style}: IMyText): JSX.Element => {
+const CustomText = ({ numberOfLines, text, style }: IMyText): JSX.Element => {
   let [fontsLoaded] = useFonts({
-    Roboto_300Light,
-    Roboto_400Regular,
-    Oswald_300Light,
     OpenSans_400Regular,
-    OpenSans_300Light,
   });
 
   if (!fontsLoaded) {
@@ -26,10 +14,7 @@ const CustomText = ({numberOfLines, text, style}: IMyText): JSX.Element => {
   }
 
   return (
-    <Text
-      numberOfLines={numberOfLines}
-      style={[styles.text, style]}
-    >
+    <Text numberOfLines={numberOfLines} style={[styles.text, style]}>
       {text}
     </Text>
   );
